307ed1f83ecd1660db6e7c3d1e056a6f58b3ccdb
hofric15 <hofric15@htlkaindorf.at>
2018-09-28 11:59:20 +0200

    added new class Schueler

---
+++ Exercise_103_Terminkalender/src/Terminkalender/Schueler.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Terminkalender;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Richard Hofmeister
+ */
+public class Schueler {
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
